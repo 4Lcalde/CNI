b85411d95d046bbc164abf5dc18acdde64187176
4Lcalde <aitorjimeneztecno@gmail.com>
2024-05-28 12:48:37 +0200

    G

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { header } from './public/SRC/components/header/header'
 import { logIn } from './public/SRC/components/logIn/logIn'
 import './style.css'
 
-const datosPlaca = localStorage.getItem('Placa')
+const datosPlaca = sessionStorage.getItem('Placa')
 
 if (datosPlaca) {
   header()
--- public/SRC/components/logIn/logIn.js
+++ public/SRC/components/logIn/logIn.js
@@ -26,81 +26,82 @@ export const logIn = () => {
 
     if (inputValue === 150222) {
       placa.Nombre = 'Beltrán'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 170305) {
       placa.Nombre = 'David'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 176985) {
       placa.Nombre = 'Cris'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 365987) {
       placa.Nombre = 'Enol'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 112536) {
       placa.Nombre = 'Víctor'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 775868) {
       placa.Nombre = 'Héctor'
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 445558) {
       placa.Nombre = 'Josemi'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 20345) {
       placa.Nombre = 'Laura'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 171825) {
       placa.Nombre = 'Luis'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 452535) {
       placa.Nombre = 'Marisa'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 96568) {
       placa.Nombre = 'Marta'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 696587) {
       placa.Nombre = 'Mayte'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 232541) {
       placa.Nombre = 'Pablo'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 182659) {
       placa.Nombre = 'Paula'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 332542) {
       placa.Nombre = 'Victoria'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else if (inputValue === 19875) {
       placa.Nombre = 'Zyme'
-      localStorage.setItem('Placa', inputValue)
+      sessionStorage.setItem('Placa', inputValue)
       divLogin.remove()
       header()
     } else {
